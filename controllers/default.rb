e3735f33064df8c4f77233d111fa34eb708c8361
Dan Yoder <dan@zeraweb.com>
2008-11-06 08:08:23 -0800

    Fixed a bug with creating new stories.

--- controllers/default.rb
+++ controllers/default.rb
@@ -8,7 +8,7 @@ module Pages
 			include Pages::ResponseMixin
 			
 			alias_method :_model, :model ; def model ; _model( model_name ) ; end
-			def create ; model.create( assigns.to_h ) ; end
+			def create ; model.create( query[ model_name ].to_h ) ; end
 			def find( name ) ; model.find( name ) or not_found ; end
 			def update( name ) ; find( name ).assign( query[ model_name ].to_h ).save ; end
 			def delete( name ) ; find( name ).delete ; end
