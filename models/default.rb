1007a213ecc9d960e785b4c72cb0f4253bd2f3d2
Krishna Krishnamaneni <kkrishnamaneni@YPCMC03347.local>
2008-11-06 18:22:22 -0800

    fixed problem with creating new story

--- models/default.rb
+++ models/default.rb
@@ -8,17 +8,22 @@ module Pages
       def self.[]( domain ) 
 			  Class.new( self ) do
 			    include( Filebase::Model[ :db / domain / superclass.basename.snake_case ] )
+          
+          alias_method :orig_assigns, :assign
+      
+          def assign( assigns )
+            assigns[ :key ] = assigns['title'].downcase.gsub(/\s+/,'-').gsub(/[^\w\-]/,'') unless get( :key )
+            assigns[ :published ] = Date.today unless assigns['published']
+            super( assigns )
+          end
+          
 			    associate( domain )
+          
 			  end
 			end
 			
 			def self.associate( domain ) ; end
 			
-			def assign( assigns )
-			  assigns[ :key ] = assigns.title.downcase.gsub(/\s+/,'-').gsub(/[^\w\-]/,'') unless get( :key )
-			  assigns[ :published ] = Date.today unless assigns.published
-			  super( assigns )
-		  end
 		  
 			def title ;  get( :title ) or '' ; end
 			
