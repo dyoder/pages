58ed76c770397a747fdfd2483d70621c5232d0eb
Krishna Krishnamaneni <kkrishnamaneni@attinteractive.com>
2009-02-25 17:24:16 -0800

    Added :ext matcher to feed

--- resources/blog.rb
+++ resources/blog.rb
@@ -1,15 +1,15 @@
 module Pages
-  
+
   module Resources
-    
+
     class Blog < Default
 
-      on( :get, { :feed => [ 'blog', :name ] }, :accept => :xml ) do
+      on( :get, { :feed => [ 'blog', :name ] }, :ext => :xml ) do
         view.feed( :blog => controller.find( basename( captured.name ) ) )
       end
-      
+
     end
-    
+
   end
 
 end
