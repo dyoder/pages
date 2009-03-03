abea0902cabd351f5c2f9446da8c9e9dc3b64c60
Roberto Gamboni <cproby2005@gmail.com>
2009-03-03 14:36:10 -0800

    added dispatched for static images of type gif and png from public dir.

--- resources/media.rb
+++ resources/media.rb
@@ -7,8 +7,8 @@ module Pages
       include Waves::Resources::Mixin
       
       on( :get, :get => [ :media, { :asset => true } ] ) do
-        # example of http freshness - js and css are valid for 24 hours - rack-cache will return the cached copy of css and js.
-        response['Cache-Control'] = 'max-age=86400'
+        # example of http freshness - js and css are valid for 1 hour - rack-cache will return the cached copy of css and js.
+        response['Cache-Control'] = 'max-age=3600'
         controller.get( captured.media, captured.asset * '/')
       end
       
