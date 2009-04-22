b8412b8996f8132a494d78902382b7b70d44edc2
Krishna Krishnamaneni <kkrishnamaneni@attinteractive.com>
2009-04-22 11:19:56 -0700

    Fixed media to work with waves/edge

--- resources/media.rb
+++ resources/media.rb
@@ -1,19 +1,19 @@
 module Pages
-  
+
   module Resources
-    
+
     class Media
-      
+
       include Waves::Resources::Mixin
-      
+
       on( :get, :get => [ :media, { :asset => true } ] ) do
         # example of http freshness - js and css are valid for 1 hour - rack-cache will return the cached copy of css and js.
         response['Cache-Control'] = 'max-age=3600'
-        controller.get( captured.media, captured.asset * '/')
+        controller.get( captured.media, (captured.asset  * '/') + (request.ext || ''))
       end
-      
+
     end
-    
+
   end
 
 end
