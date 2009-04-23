8ad84500c772316dbc0d2cd8547c9f804bff7792
Krishna Krishnamaneni <kkrishnamaneni@attinteractive.com>
2009-04-23 12:29:07 -0700

    Fixed upload when updating the file

--- models/image.rb
+++ models/image.rb
@@ -12,12 +12,11 @@ module Pages
         Class.new( self ) do
           include( Filebase::Model[ :db / domain / superclass.basename.snake_case ] )
           associate( domain )
-          
+
           # callback from filebase before saving
           before_save do |obj|
-            # TODO Fix this...hack doesn't seem to work
-            # Hack - for recognizing updating vs creating - file object in html contains a different value if set programatically.
-            if !obj.file.is_a? ::Filebase::YAML::DomainType 
+            #Save the file only if file is uploaded.
+            if obj.file and not obj.file['filename'].empty?
               rel_path = 'files'
               dirpath = :db / domain / superclass.basename.snake_case / rel_path
               ::FileUtils.mkdir_p(dirpath) unless File.exists?(dirpath)
@@ -26,10 +25,11 @@ module Pages
               obj.filepath = rel_path / obj.key
               obj.content_type = obj.file['type']
               obj.orig_name = obj.file['filename']
+              obj.file = nil
             end
             obj
           end
-          
+
           # callback from filebase before deleting
           before_delete do |obj|
             g = Pages::Models[ 'gallery' ][ domain ].find( obj.gallery )
@@ -39,7 +39,7 @@ module Pages
             end
             obj
           end
-          
+
         end
       end
 
