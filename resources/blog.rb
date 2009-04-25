8105ff439ff08a3f68ef0c82af4c7db913c3f0c4
Roberto Gamboni <cproby2005@gmail.com>
2009-04-25 11:31:27 -0700

    re-added blog feed generated using blog.xml or blog.rss

--- resources/blog.rb
+++ resources/blog.rb
@@ -5,7 +5,7 @@ module Pages
     class Blog < Default
 
       # feed removed till we have a more understanding of extension matching      
-      on( :get, { :feed => [ 'blog', :name ] }, :ext => [ :xml ] ) do
+      on( :get, { :feed => [ 'blog', :name ] }, :ext => [ :xml, :rss ] ) do
         view.feed( :blog => controller.find( basename( captured.name ) ) )
       end
 
