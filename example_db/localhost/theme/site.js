3506a85ba9d342393f616f4411c400d892a3259f
Roberto Gamboni <cproby2005@gmail.com>
2009-04-29 15:24:33 -0700

    added valid example_db

---
+++ example_db/localhost/theme/site.js
@@ -0,0 +1,7 @@
+//$(document).ready( function() { 
+// $.ajax({ type: 'get', url: '/social/twitter-this',
+//    beforeSend : function(xhr) {  xhr.setRequestHeader('Accept','text/html-fragment') },
+//    success: function( html ) {
+//     $('div.twitter_this').replaceWith( html ); 
+//  }});
+//});
